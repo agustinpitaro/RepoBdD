48bd9e00870bc92505245035b1e8ad11085417d1
Agustin Pitaro <agustin.pitaro@gmail.com>
2020-12-04 19:38:26 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { ProductoModule } from './producto/producto.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ProductoModule,
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
